41d8ce62df2da2356766e17c0d869690bc6456f2
Gedler Jean Pierre <gedler.jeanpierre@stonybrook.edu>
2021-08-16 16:42:22 -0400

    postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_07_15_014502) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "artists", force: :cascade do |t|
     t.string "name"
     t.string "location"
